4085de23b7c07e6c82efeb4a62cd8c0e5386f883
Joe Prathap P J <joeprathappj@gmail.com>
2024-03-20 07:13:32 +0530

    initial commit

---
+++ config/connection.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+mongoose.set("strictQuery", false);
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb+srv://joeprathap123:joeprathap123@cluster0.qgfc2.mongodb.net/fitness", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}, err => {
+  if (err) throw err;
+  console.log('Connected to MongoDB!')
+}
+
+);
+
+module.exports = mongoose.connection;
+
+
+// mongodb+srv://sivasriram1206:siva1206@fitness.pzycuer.mongodb.net
